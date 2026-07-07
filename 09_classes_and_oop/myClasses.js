29bf485c0272afa23c4d74a13acc12e810a09d6b
aliza-dev <alizait1192@gmail.com>
2026-07-08 00:04:36 +0500

    classes & constructor

---
+++ 09_classes_and_oop/myClasses.js
@@ -0,0 +1,56 @@
+// ES6
+
+class User {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+
+}
+
+const chai = new User("chai", "chai@gmail.com", "123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
+
+
+/*
+
+Notes:
+
+new Keyword and Constructor:
+
+As soon as you create (initialize) an object from a class, the constructor gets called automatically.
+This happens the moment you use the new keyword.
+In other words: new keyword → constructor runs immediately — you don't need to call it separately, it's automatic as part of object creation.
+
+*/
